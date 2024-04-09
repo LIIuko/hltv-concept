951d1489a2fbc9cd47662bc6401ee3830874c5db
vyacheslavbotoev <botoevslava27032003@mail.ru>
2024-04-09 13:54:40 +0300

    Added top teams list

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,21 +1,38 @@
-import styles from "./Team.module.scss";
 import PlayersList from "../PlayersList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import styles from "./Team.module.scss";
 
 const Team = ({ teams }) => {
-    const [isOpened, setIsOpened] = useState(false);
+    const [openedId, setOpenedId] = useState(null);
+
+    const handleTeamClick = (teamId) => {
+        setOpenedId(openedId === teamId ? null : teamId);
+    };
+
     return (
-        <>
-            {teams.map((item, index) => (
-                <div className={styles.container}>
-                    <h2>#{index + 1}</h2>
-                    <img src={item.image} alt="Team logo" />
-                    <h3>{item.team}</h3>
-                    <span>{item.points} points</span>
-                    <PlayersList players={item.players} isOpened={isOpened} />
-                </div>
+        <ul>
+            {teams.map((team, index) => (
+                <li
+                    key={team.team}
+                    className={styles.container}
+                    onClick={() => handleTeamClick(team.team)}>
+                    <div className={styles.number}>
+                        <h2>#{index + 1}</h2>
+                    </div>
+                    <div className={styles.name}>
+                        <img src={team.image} alt="Team logo" />
+                        <h3>{team.team}</h3>
+                        <span>({team.points} points)</span>
+                    </div>
+                    <div className={styles.players__list}>
+                        {openedId === team.team && (
+                            <PlayersList players={team.players} />
+                        )}
+                    </div>
+                </li>
             ))}
-        </>
+        </ul>
     );
 };
 
